a5bfcf434cd8498edf3aba2f683edfdaa2eee364
mahikero <federicoparassarmiento@yahoo.com>
2016-08-16 16:56:09 -0400

    up

---
+++ 04 Using Angular's Built In Directive/scripts/app.js
@@ -0,0 +1,12 @@
+angular.module("todoListApp", [])
+.controller('mainCtrl', function($scope){
+	$scope.helloWorld = function() {
+		console.log("Hello there! This the helloWorld controller function, in the mainCtrl!");
+	};
+})
+
+.controller('coolCtrl', function($scope){
+	$scope.whoAmI = function() {
+		console.log("hello there, this is the coolCtrl function!");
+	};
+});
